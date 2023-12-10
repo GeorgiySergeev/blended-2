7ac6d9907bc395e8d3ebd76b3d93bc67cf5f3fa8
turasova <134943646+turasova@users.noreply.github.com>
2023-12-10 12:30:33 +0200

    task1

--- src/tabs/Gallery.jsx
+++ src/tabs/Gallery.jsx
@@ -8,6 +8,8 @@ export class Gallery extends Component {
     images: [],
     query: '',
     page: 1,
+    loadMore: false,
+    error: null,
   };
   componentDidMount() {
     ImageService.getImages('car', 2);
@@ -19,9 +21,15 @@ export class Gallery extends Component {
     try {
       const images = await ImageService.getImages(query, page);
       console.log(images);
-      this.setState({ images: images.photos });
+      this.setState(prevState => ({
+        images: [...prevState.images, ...images.photos],
+        loadMore: images.page < Math.ceil(images.total_results/images.per_page)
+      }));
+
     } catch (error) {
-      console.log(error);
+      this.setState({
+       error: error.message,
+     })
     }
   };
 
@@ -34,14 +42,41 @@ export class Gallery extends Component {
   }
 
   onFormSubmit = newQuery => {
-    this.setState({ query: newQuery });
+    this.setState({
+      query: newQuery,
+      images: [],
+      page: 1,
+      error: null,
+    });
+
   };
 
+  onLoadMore = () => {
+    this.setState(prevState => {
+      return {
+        page: prevState.page + 1,
+      }
+    })
+  }
+
   render() {
+    const { images, loadMore, error } = this.state;
     return (
       <>
         <SearchForm onSubmit={this.onFormSubmit} />
-        <Text textAlign="center">Sorry. There are no images ... 😭</Text>
+        <Grid>
+          {images.map(({ id, avg_color, alt, src }) => {
+            return <GridItem key={id}>
+                       <CardItem color={avg_color}>
+                          <img src={src.large} alt={alt} />
+                      </CardItem>
+                 </GridItem>
+          })}
+
+        </Grid>
+        {loadMore && <Button type="button" onClick={this.onLoadMore}>Load More</Button>}
+        {error && <Text textAlign="center">Sorry. There are no images ... 😭</Text>}
+
       </>
     );
   }
